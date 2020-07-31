5f8841beb4c87b66ddf30ee8d4a4aa4cb4ef2932
Miyasaka Koki <s15006@al.kic.ac.jp>
2020-08-01 01:34:29 +0900

    Add PizzaParty Command! (#1)
    たぶんいける?

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 import requests
 import json
 
+import datetime
+
 VERSION    = os.environ['VERSION']
 TOKEN      = os.environ['DISCORD_TOKEN']
 CHANNEL_ID = os.environ['DISCORD_CHANNEL_ID']
@@ -25,12 +27,24 @@ async def on_message(message):
         await message.channel.send("Bot Version: " + str(VERSION) + "\n" )
     if message.content == "/pp":
         await message.channel.send(message.channel, file=discord.File("img/pp.gif","pp.gif"))
-    
+    if message.content[0:7]== "/pizza ":
+        await message.channel.send(message.channel, get_next_pizza_party )
+
 def get_help_text():
     res = "Pastime Bot ver" + str(VERSION) + "\n"
     res += "[コマンド一覧]\n"
     res += "/help\t\t ヘルプを表示\n"
     res += "/pp\t\t\t God Command\n"
     return res
+
+def get_next_pizza_party():
+    now = datetime.datetime.now()
+    if now.weekday() == 5:
+        return "Today!"
+    if now.weekday() == 4:
+        return "Tomorrow!"
+    else:
+        np = now + datetime.timedelta(days=(5-now.weekday())%7)
+        return np.strftime("%Y/%M/%D (Sat) !")
 
 client.run(TOKEN)
